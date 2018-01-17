ca6abc19ddfc14f4264c3548cc5f8a45fa51b3b5
Pierre Lebrun <pierreyves.lebrun@gmail.com>
2018-01-17 11:52:41 +0900

    Renamed variables to conform to Rubocop rules

--- lib/brick_ftp/api/file_operation/upload.rb
+++ lib/brick_ftp/api/file_operation/upload.rb
@@ -44,22 +44,22 @@ module BrickFTP
           new(step1.merge(step3).symbolize_keys)
         end
         
-        def self.step1(path:, ref: nil, partNumber: nil)
+        def self.step1(path:, ref: nil, part_number: nil)
           api_client = BrickFTP::HTTPClient.new
 
-          if ref.nil? || partNumber.nil?
+          if ref.nil? || part_number.nil?
             step1 = api_client.post(api_path_for(:create, path: path), params: { action: 'put' })
           else
-            step1 = api_client.post(api_path_for(:create, path: path), params: { action: 'put', ref: ref, part: "#{partNumber}" })
+            step1 = api_client.post(api_path_for(:create, path: path), params: { action: 'put', ref: ref, part: part_number })
           end
 
           step1.symbolize_keys
         end
 
-        def self.step2(source:, uploadUri:)
-          upload_uri = URI.parse(uploadUri)
-          upload_client = BrickFTP::HTTPClient.new(upload_uri.host)
-          upload_client.put(uploadUri, params: source)
+        def self.step2(source:, upload_uri:)
+          upload_parsed_uri = URI.parse(uploadUri)
+          upload_client = BrickFTP::HTTPClient.new(upload_parsed_uri.host)
+          upload_client.put(upload_uri, params: source)
         end
 
         def self.step3(path:, ref:)
